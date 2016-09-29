15162358af442f9ce99dc144491995160444944d
Daher Alfawares <daher.alfawares@live.com>
2016-09-29 14:18:50 -0500

    adding feature

---
+++ Feature/Feature.swift
@@ -0,0 +1,13 @@
+//
+//  Feature.swift
+//  Feature
+//
+//  Created by Daher Alfawares on 9/29/16.
+//  Copyright © 2016 Solstice Mobile. All rights reserved.
+//
+
+import Foundation
+
+public class Feature {
+    public let featureName = "Feature Name From Code"
+}
