701a8a5993145d538359128d9c73d61eb0c264cb
Daher Alfawares <daher.alfawares@live.com>
2016-09-29 11:17:56 -0500

    adding

---
+++ Feature/Feature.h
@@ -0,0 +1,19 @@
+//
+//  Feature.h
+//  Feature
+//
+//  Created by Daher Alfawares on 9/28/16.
+//  Copyright © 2016 Solstice Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Feature.
+FOUNDATION_EXPORT double FeatureVersionNumber;
+
+//! Project version string for Feature.
+FOUNDATION_EXPORT const unsigned char FeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feature/PublicHeader.h>
+
+
